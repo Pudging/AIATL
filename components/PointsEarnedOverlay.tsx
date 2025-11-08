36b7fb0d536619a2670da274e34a7cbf260a779a
Kevin Gao <91993597+Pudging@users.noreply.github.com>
2025-11-08 16:57:27 -0500

    Update PointsEarnedOverlay.tsx

--- components/PointsEarnedOverlay.tsx
+++ components/PointsEarnedOverlay.tsx
@@ -9,6 +9,7 @@ type Props = {
 };
 // comment
 //comment
+//comment??
 export default function PointsEarnedOverlay({ show, points, label }: Props) {
   const isPositive = points >= 0;
   const absPoints = Math.abs(points);
